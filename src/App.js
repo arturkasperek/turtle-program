ffae45ed3e8dc994d1f7e9a279fc036f3673b963
Artur Kasperek <arturmiks@gmail.com>
2019-10-13 17:08:57 +0200

    Added prettier (#12)

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React from 'react';
 import './App.scss';
 
 function App() {
-  return (
-    <div className="app-container">
-      Hello Turtle!
-    </div>
-  );
+  return <div className='app-container'>Hello Turtle!</div>;
 }
 
 export default App;
