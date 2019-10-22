be16d519a3c52be90ba56bcdf6aca9c63318cb58
Szymek <szymonjoszko@gmail.com>
2019-10-22 14:35:11 +0200

    Basic layout #issue4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import './App.scss';
+import { spawn } from 'child_process';
 
 function App() {
-  return <div className='app-container'>Hello Turtle!</div>;
+    return (
+        <main>
+            <div className = "panels">Canvas</div>
+            <div className = "panels" id="rightPanel">
+                <div id = "textEditor">Text editor</div>
+                <div id = "bottomPanel">
+                    <button id="runButton">Run</button>
+                    <div id="rangeDiv">
+                        <div id="turtleImage"></div>
+                        <input id="range" type="range" name="" min="0" max="100" />
+                        <div id="rabbitImage"></div>
+                    </div>
+                </div>
+            </div>
+        </main>
+     );
 }
 
 export default App;
