43c936938dfccfe6c9e57af665e5800558753327
Artur Kasperek <arturmiks@gmail.com>
2019-10-22 16:25:17 +0200

    Basic layout #issue4 (#18)
    * Basic layout #issue4
    
    * Added ace editor
    
    * Style improvement
    
    * Fixed fomrating
    
    * Fixed formatting
    
    * Fixed tests

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import './App.scss';
+import Canvas from './components/Canvas';
+import Editor from './components/Editor';
+import Control from './components/Control';
 
 function App() {
-  return <div className='app-container'>Hello Turtle!</div>;
+  return (
+    <main>
+      <Canvas />
+      <div className='panels' id='rightPanel'>
+        <Editor />
+        <Control />
+      </div>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/Canvas.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../App.scss';
+
+class Canvas extends React.Component {
+  canvasRef = React.createRef();
+
+  componentDidMount() {
+    const canvas = this.canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    console.log('ctx is ', ctx);
+  }
+
+  render() {
+    return (
+      <div className='panels'>
+        <canvas ref={this.canvasRef} />
+      </div>
+    );
+  }
+}
+
+export default Canvas;
