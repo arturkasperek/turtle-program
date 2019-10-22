afe7ce13018476df3e6960eabc29e9409b9dbc78
Artur <arturmiks@gmail.com>
2019-10-22 15:55:50 +0200

    Fixed formatting

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
 import React from 'react';
-import AceEditor from "react-ace";
+import AceEditor from 'react-ace';
 import './App.scss';
 
 function App() {
-    return (
-        <main>
-            <div className = "panels">Canvas</div>
-            <div className = "panels" id="rightPanel">
-                <div id = "textEditor">
-                  <AceEditor
-                    mode="java"
-                    theme="github"
-                    onChange={(value)=>{console.log('value is ', value)}}
-                    name="ACE_EDITOR"
-                    editorProps={{ $blockScrolling: true }}
-                  />
-                </div>
-                <div id = "bottomPanel">
-                    <button id="runButton">Run</button>
-                    <div id="rangeDiv">
-                        <div id="turtleImage"></div>
-                        <input id="range" type="range" name="" min="0" max="100" />
-                        <div id="rabbitImage"></div>
-                    </div>
-                </div>
-            </div>
-        </main>
-     );
+  return (
+    <main>
+      <div className='panels'>Canvas</div>
+      <div className='panels' id='rightPanel'>
+        <div id='textEditor'>
+          <AceEditor
+            mode='java'
+            theme='github'
+            onChange={(value) => {
+              console.log('value is ', value);
+            }}
+            name='ACE_EDITOR'
+            editorProps={{ $blockScrolling: true }}
+          />
+        </div>
+        <div id='bottomPanel'>
+          <button id='runButton'>Run</button>
+          <div id='rangeDiv'>
+            <div id='turtleImage'></div>
+            <input id='range' type='range' name='' min='0' max='100' />
+            <div id='rabbitImage'></div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
