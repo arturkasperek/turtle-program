db89a7a6dc6aa0b3efe291d298189dfa5ede517b
Wiktor Hosumbek <wiktor4444@gmail.com>
2019-11-05 14:33:35 +0100

    Add format

--- src/components/Control.js
+++ src/components/Control.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../App.scss';
-import slowIcon from '../img/snail.png'
-import fastIcon from '../img/rabbit.png'
+import slowIcon from '../img/snail.png';
+import fastIcon from '../img/rabbit.png';
 
 class Control extends React.Component {
   render() {
@@ -9,14 +9,17 @@ class Control extends React.Component {
       <div id='bottomPanel'>
         <button id='runButton'>Run</button>
         <div id='rangeDiv'>
-                <div id='turtleImage'><img src={slowIcon} width="32" height="32" /></div>
+          <div id='turtleImage'>
+            <img src={slowIcon} width='32' height='32' />
+          </div>
           <input id='range' type='range' name='' min='0' max='100' />
-                <div id='rabbitImage'><img src={fastIcon} width="25" height="25" /></div>
+          <div id='rabbitImage'>
+            <img src={fastIcon} width='25' height='25' />
+          </div>
         </div>
       </div>
     );
   }
 }
-
 
 export default Control;
