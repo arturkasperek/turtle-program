f98ca278088e16f0fef30829864429f2572f30ed
Artur Kasperek <arturmiks@gmail.com>
2019-11-26 21:29:14 +0100

    Added basic drawing commands on canvas (#26)
    * Added basic drawing commands on canvas
    
    * Prettier fixes

--- public/electron.js
+++ public/electron.js
@@ -8,7 +8,7 @@ const isDev = require('electron-is-dev');
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680, webPreferences: {
+  mainWindow = new BrowserWindow({width: 1100, height: 680, webPreferences: {
       nodeIntegration: true
     }});
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
