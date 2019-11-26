3192e77ea483c164706f8aa19dfa3a6d98dd0868
Artur <arturmiks@gmail.com>
2019-11-26 21:23:43 +0100

    Added basic drawing commands on canvas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.scss';
-import Canvas from './components/Canvas';
+import CanvasDrawer from './components/CanvasDrawer';
 import Editor from './components/Editor';
 import Control from './components/Control';
 
 function App() {
   return (
     <main>
-      <Canvas />
+      <CanvasDrawer />
       <div className='panels' id='rightPanel'>
         <Editor />
         <Control />
---
+++ src/components/CanvasDrawer.js
@@ -0,0 +1,157 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import './CanvasDrawer.scss';
+
+class CanvasDrawer extends Component {
+  canvasRef = React.createRef();
+  canvasDrawerRef = React.createRef();
+  currentPos = {
+    x: 100,
+    y: 100,
+  };
+  turtleAngle = 0;
+  speed = 1;
+  oldToDraw = [];
+  canvasSize = {
+    x: 0,
+    y: 0,
+  };
+
+  drawAnimate = (ctx, width, onDraw) => {
+    return new Promise((resolve) => {
+      const timeOfDrawing = (width / 100) * this.speed * 1000;
+      let start = null;
+      let pos;
+      const animate = (time) => {
+        ctx.clearRect(0, 0, this.canvasSize.x, this.canvasSize.y);
+        this.oldToDraw.forEach(func => func());
+        if (!start) {
+          start = time;
+        };
+        const timeProgress = time - start;
+        const progress = timeProgress / timeOfDrawing;
+        pos = onDraw(progress);
+
+        if (timeProgress < timeOfDrawing) {
+          window.requestAnimationFrame(animate);
+        } else {
+          this.oldToDraw.push(() => onDraw(1));
+          this.currentPos = pos;
+          resolve();
+        }
+      };
+      window.requestAnimationFrame(animate);
+    });
+  };
+
+  drawLineAnimate = (ctx, width) => {
+    const drawFunc = (widthP, angleP, currentPos) => {
+      return (progress) => this.drawLine(ctx, widthP * progress, angleP, currentPos);
+    };
+    return this.drawAnimate(ctx, width, drawFunc(width, this.turtleAngle, {...this.currentPos}));
+  };
+
+  drawArcAnimate = (ctx, percentageToDraw, r) => {
+    const l = 2 * Math.PI * r;
+    const drawFunc = (percentageToDrawP, rP, currentPos) => {
+      return (progress) => this.drawArc(ctx, percentageToDrawP * progress, rP, currentPos);
+    };
+
+    return this.drawAnimate(ctx, l * percentageToDraw / 100, drawFunc(percentageToDraw, r, this.currentPos));
+  };
+
+  async componentDidMount() {
+    const canvas = this.canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    const adjustCanvasSize = this.adjustCanvasSizeFactory(ctx);
+
+    window.addEventListener('resize', adjustCanvasSize);
+    adjustCanvasSize();
+
+    await this.drawLineAnimate(ctx, 100);
+    this.rotate(50);
+    await this.drawLineAnimate(ctx, 50);
+    this.rotate(90);
+    await this.drawLineAnimate(ctx, 50);
+    this.rotate(90);
+    await this.drawArcAnimate(ctx, 20, 100);
+    await this.drawArcAnimate(ctx, 20, 20);
+    this.rotate(40);
+    await this.drawLineAnimate(ctx, 50);
+    this.rotate(90);
+    await this.drawLineAnimate(ctx, 100);
+  }
+
+  adjustCanvasSizeFactory = (ctx) => {
+    return () => {
+      const sizeX = this.canvasDrawerRef.current.offsetWidth;
+      const sizeY = this.canvasDrawerRef.current.offsetHeight;
+
+      this.canvasRef.current.width = sizeX;
+      this.canvasRef.current.height = sizeY;
+
+      this.canvasSize = {
+        x: sizeX,
+        y: sizeY,
+      };
+
+      ctx.clearRect(0, 0, this.canvasSize.x, this.canvasSize.y);
+      this.oldToDraw.forEach(func => func());
+    };
+  };
+
+  drawArc(ctx, percentageToDraw, r, currentPos) {
+    const x1 = currentPos.x;
+    const y1 = currentPos.y;
+    const toDraw = percentageToDraw / 100;
+    const c1 = x1 + r;
+    const c2 = y1;
+    const angle = (1 + 2 * toDraw) * Math.PI;
+    const x2 = c1 + Math.cos(angle) * r;
+    const y2 = c2 + Math.sin(angle) * r;
+
+    ctx.beginPath();
+    ctx.arc(c1, c2, r, Math.PI, angle);
+    ctx.stroke();
+
+    return {
+      x: x2,
+      y: y2,
+    };
+  }
+
+  drawLine(ctx, width, turtleAngle, currentPos) {
+    const x1 = currentPos.x;
+    const y1 = currentPos.y;
+    const r =  width;
+    const theta = Math.PI * turtleAngle;
+    const x2 = x1 + r * Math.cos(theta);
+    const y2 = y1 + r * Math.sin(theta);
+    ctx.lineCap = 'round';
+    ctx.lineWidth = 2;
+
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+
+    return {
+      x: x2,
+      y: y2,
+    };
+  }
+
+  rotate(angle) {
+    this.turtleAngle+= angle / 180;
+  }
+
+  render() {
+    return (
+      <div className={'canvas-drawer'} ref={this.canvasDrawerRef}>
+        <canvas width={'395px'} height={'646px'} ref={this.canvasRef} />
+      </div>
+    );
+  }
+}
+
+export default CanvasDrawer;
