da3ffe1c26c3eb1b10773562c110714236d0aaa6
bullts <52421507+bullts@users.noreply.github.com>
2019-12-10 15:20:05 +0100

    add speed control (#36)

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ import Control from './components/Control';
 function App() {
   const [drawFunctions, setDrawFunctions] = useState({});
   const [commands, setCommands] = useState([]);
+  const [speed, setSpeed] = useState(50);
 
   return (
     <main>
-      <CanvasDrawer getDrawingRef={(drawingFunctions) => setDrawFunctions(drawingFunctions)} />
+      <CanvasDrawer speed={speed} getDrawingRef={(drawingFunctions) => setDrawFunctions(drawingFunctions)} />
       <div className='panels' id='rightPanel'>
         <Editor setCommands={setCommands} />
-        {!isEmpty(drawFunctions) && <Control commands={commands} drawFunctions={drawFunctions} />}
+        {!isEmpty(drawFunctions) && <Control speed={speed} setSpeed={setSpeed} commands={commands} drawFunctions={drawFunctions} />}
       </div>
     </main>
   );
