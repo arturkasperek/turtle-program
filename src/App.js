fcbb9a207506554ce0a48449c262fa41e2581c26
SzymonJoszko <48184291+SzymonJoszko@users.noreply.github.com>
2020-01-16 02:39:14 +0100

    Issue55 Mobile layout  (#59)
    * Mobile layout version added
    
    * Proportion between panels changed 60:40
    
    * Conflict resolved correctly

--- src/App.js
+++ src/App.js
@@ -9,23 +9,36 @@ function App() {
   const [drawFunctions, setDrawFunctions] = useState({});
   const [commands, setCommands] = useState([]);
   const [speed, setSpeed] = useState(50);
+  const [display, setDisplay] = useState({ drawer: 'grid', rightPanel: 'grid' });
   const [runActive, setRunActive] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
 
   return (
     <main>
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+
       <CanvasDrawer
-        setRunActive={setRunActive}
-        errorMessage={errorMessage}
         speed={speed}
         getDrawingRef={(drawingFunctions) => setDrawFunctions(drawingFunctions)}
         commands={commands}
+        setRunActive={setRunActive}
+        errorMessage={errorMessage}
+        display={display}
+        setDisplay={setDisplay}
       />
 
-      <div className='panels' id='rightPanel'>
+      <div className='panels' id='rightPanel' style={{ display: display.rightPanel }}>
         <Editor setErrorMessage={setErrorMessage} setCommands={setCommands} setRunActive={setRunActive} />
         {!isEmpty(drawFunctions) && (
-          <Control speed={speed} setSpeed={setSpeed} commands={commands} drawFunctions={drawFunctions} runActive={runActive} />
+          <Control
+            speed={speed}
+            setSpeed={setSpeed}
+            commands={commands}
+            drawFunctions={drawFunctions}
+            runActive={runActive}
+            display={display}
+            setDisplay={setDisplay}
+          />
         )}
       </div>
     </main>
