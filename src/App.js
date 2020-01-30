54d42c4223969c614123bf11055f2e1103a2f1f8
Artur <arturmiks@gmail.com>
2020-01-30 16:49:07 +0100

    Broken

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react';
 import { isEmpty } from 'lodash';
 import './App.scss';
@@ -5,7 +6,6 @@ import CanvasDrawer from './components/CanvasDrawer';
 import Editor from './components/Editor';
 import Control from './components/Control';
 
-function App() {
   const [drawFunctions, setDrawFunctions] = useState({});
   const [commands, setCommands] = useState([]);
   const [speed, setSpeed] = useState(50);
